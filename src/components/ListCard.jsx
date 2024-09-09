6aca3ec8cf212ac7607412b0ba4d1d0715d1a117
Yoos99 <yoosh2520@naver.com>
2024-09-09 10:14:44 +0900

    feat: 네비게이트 추가

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,13 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import style from '../css/ListCard.module.css';
 
+{
+  /*props 이름은 일치해야함 <ListCard item={someProductData} /> 이라면 item이 props여서 item으로 받아야하는 것  */
+}
 export default function ListCard({ product }) {
+  const navigate = useNavigate();
+  const goDetail = () => {
+    navigate(`/detail/${product.id}`);
+  };
+
   return (
-    <li className={style.ListCard}>
+    <li className={style.ListCard} onClick={goDetail}>
       <div>
         <div className={style.cardImg}>
-          {/* 절대경로로 이미지 폴더를 받아오고 product의 이미지 가져옴  */}
-          <img src={`/img/${product.img}`} alt={`${product.title} `} />{' '}
-          {/*alt는 이미지의 대체 텍스트를 나타냅니다.*/}
+          {/* 절대경로로 이미지 폴더를 받아오고 product의 이미지 가져옴 alt는 이미지의 대체 텍스트를 나타냅니다. */}
+          <img src={`/img/${product.img}`} alt={product.title} />
+
+          {}
         </div>
         <div className={style.cardInfo}>
           <p>{product.title}</p>
@@ -15,6 +25,7 @@ export default function ListCard({ product }) {
         </div>
         <span className={style.discount}>{product.discount}%</span>
       </div>
+      {/* <Link to="/detail">자세히 보기</Link> */}
     </li>
   );
 }
