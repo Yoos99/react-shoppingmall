ff18e5db1ceeb4eaeecb646bc57c66e156314196
Yoos99 <yoosh2520@naver.com>
2024-09-11 00:14:15 +0900

    refactor: import 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,24 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
 import Footer from './layout/Footer';
 import Header from './layout/Header';
 import MainPage from './pages/MainPage';
 import ShopPage from './pages/ShopPage';
 import BlogPage from './pages/BlogPage';
 import OurPage from './pages/OurPage';
-import CartPage from './pages/CartPage';
 import DetailPage from './pages/DetailPage';
+import CartPage from './pages/CartPage';
 
 function App() {
-  const [products, setProducts] = useState([]); //상태 정의
-  const [moreInfo, setMoreInfo] = useState({}); //상태 정의
-
-  const location = useLocation();
-  const BASE_URL = 'http://localhost:8000/products'; //기본 url 버전
-
-  const getProducts = useCallback(
-    async (page = 1, perPage = 6, sort = '', category = '') => {
-      try {
-        // let url = 'http://localhost:8000/products'; //기본 url 버전
-        let url = `${BASE_URL}?_page=${page}&_per_page=${perPage}&_sort=${sort}`;
-        // Get요청 쿼리 스트링으로 1페이지당 6개만 가져오기 + 신상품만 가져오기 / cartegory=new대신에 _sort=-discount하면 할인율 높은 순으로 정렬
-        if (category) {
-          url += `&category=${category}`;
-        }
-        let res = await fetch(url);
-        let data = await res.json();
-        setMoreInfo({
-          first: data.first,
-          prev: data.prev,
-          next: data.next,
-          last: data.last,
-          page: data.page,
-          total: data.items,
-        });
-        // setProducts(data); //기본 url 버전 상태 업데이트
-        setProducts(data.data); //쿼리 스트링 바꿨을때 데이터 형식 확인하고 바꿔주기
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    []
-  );
-
-  //한 번만 실행하여 데이터 가져오기
-  useEffect(() => {
-    if (location.pathname === '/shop') {
-      getProducts(1, 8, 'latest');
-    } else if (location.pathname === '/') {
-      getProducts(1, 6);
-    }
-    getProducts();
-  }, [location.pathname, getProducts]);
-
   return (
     <div className="wrap">
       <Header />
       <Routes>
-        <Route path="/" element={<MainPage products={products} />} />
-        <Route
-          path="/shop"
-          element={
-            <ShopPage
-              products={products}
-              getProducts={getProducts}
-              moreInfo={moreInfo}
-            />
-          }
-        />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/shop" element={<ShopPage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/our" element={<OurPage />}>
-          <Route path="ceo" element={'CEO 페이지'} />
+          <Route index element={'CEO 페이지'} />
           <Route path="history" element={'History 페이지'} />
           <Route path="org" element={'Organization 페이지'} />
         </Route>
@@ -79,6 +26,7 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/mypage" element={'마이페이지'} />
         <Route path="/detail/:id" element={<DetailPage />} />
+
         <Route
           path="*"
           element={
@@ -88,16 +36,15 @@ function App() {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                backgroundColor: '#f1f1f1',
+                backgroundColor: 'lightgray',
               }}
             >
-              올바른 주소가 아니에요
+              내용이 없어요
             </section>
           }
         />
       </Routes>
-      {/* <MainPage products={products} />  */
-      /* props 전달 */}
+
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { store } from './store/store';
-import { Provider } from 'react-redux';
-
-import './css/my-reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/my_reset.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './css/App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
