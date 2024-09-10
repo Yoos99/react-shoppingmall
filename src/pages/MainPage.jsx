5f9a371f726c16f2a5772bc74a19838e6a436e12
Yoos99 <yoosh2520@naver.com>
2024-09-11 00:56:40 +0900

    style: header

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,12 @@
-import BannerList from "../layout/BannerList";
-import ProductsList from "../layout/ProductsList";
+import BannerList from '../layout/BannerList';
+import ProductsList from '../layout/ProductsList';
 
 export default function MainPage() {
-	return (
-		<main className="mw">
-			<h2>메인페이지</h2>
-			<BannerList />
-			<ProductsList />
-		</main>
-	);
+  return (
+    <main className="mw">
+      <h2></h2>
+      <BannerList />
+      <ProductsList />
+    </main>
+  );
 }
